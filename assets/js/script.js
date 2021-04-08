e712fa2955f24140e84286ef4cf586a8bf6f385d
JoKing <joebryant614@gmail.com>
2021-04-07 21:58:50 -0700

    added drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,19 +108,19 @@ $(".list-group").on("click", "span", function () {
 $(".list-group").on("blur", "input[type='text']", function () {
   // get current text
   var date = $(this)
-  .val()
-  .trim();
+    .val()
+    .trim();
 
   // get the parent ul's id attribute
   var status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-","");
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
 
   // get the task's position in the list of the other li  elements
   var index = $(this)
-  .closest(".list-group-item")
-  .index();
+    .closest(".list-group-item")
+    .index();
 
   // update task arrau and re-save to localstorage 
   tasks[status][index].date = date;
@@ -128,8 +128,8 @@ $(".list-group").on("blur", "input[type='text']", function () {
 
   // recreate span element with bootstrap classes
   var taskSpan = $("<span>")
-  .addClass("badge badge-primary badge-pill")
-  .text(date);
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
@@ -178,6 +178,77 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+
+// make task colums sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the tamp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+      console.log(tempArr);
+    });
+     // trim down list's ID to match object property
+     var arrName = $(this)
+     .attr("id")
+     .replace("list-", "");
+ 
+   // update array on tasks object and save
+   tasks[arrName] = tempArr;
+   saveTasks();
+  }
+});
+
+// trash droppable 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+
+
+});
 // load tasks for the first time
 loadTasks();
 
